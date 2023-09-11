7e898f7a8e29fdbe6dd0d65e9ea1b1c114a3caa7
Zach Harvey <harveyzr@gmail.com>
2023-09-11 18:30:11 -0400

    working pass-gen

--- Develop/script.js
+++ Develop/script.js
@@ -3,24 +3,29 @@ var generateBtn = document.querySelector("#generate");
 
 //add rest of alphabet 
 var lowerstring = "abcdefghijklmnopqrstuvwxyz";
-
-//touppercase() method
 var upperstring = lowerstring.toUpperCase();
+
 function generatePassword() {
   var passwordLength = parseInt(prompt("Please enter the number of chracters you want for new password. It must be more than 8 but less than 128."));
   if(passwordLength < 8 || passwordLength > 128){
     alert("password length must be greater than 8, and less than 128");
     return "Try Again!"
   }
-  var lowerCases = confirm("Do you want numbers in your password?");
+ 
+  var lowerCases = confirm("Do you want lowercases in your password?");
   var upperCases = confirm("Do you want uppercases in your password");
   var numbers = confirm("Do you want numbers in your password?");
   var special = confirm("Do you want special characters in your password?");
   console.group (lowerCases, upperCases, numbers, special)
+  
+ 
+ 
   if (lowerCases === false && upperCases === false && numbers === false && special === false){
-    alert("password length must be greater than 8, and less than 128");
+    alert("You must say yes to one of the previous password characteristics");
     return "Try Again!"
   }
+  
+  
   var totalstring = ""
   if (lowerCases){
     totalstring += lowerstring
@@ -41,48 +46,16 @@ function generatePassword() {
   }
   return passString
 }
-// if(confirm("would you like lowercase letters")){//add lowercase string to empty string using concat}
-// return "fake password"
-// }
 
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
-  var password = generatePassword("Zach","Amanda");
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Special Characters for function
-const specialCharacters = "!@#$%^&*()";
 
-//Prompts that come up after you click generate password
-
-//Minimum count
-var minimumCount = 0;
-
-var minimumNumbers = "";
-var minimumLowerCases = "";
-var minimumUpperCases = "";
-var minimumSpecialCharacters = "";
-
-//Generate 
-var functionArray = [ 
-  function getNumbers() {
-    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-  },
-  function getLowerCases() { 
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-  },
-  function getUpperCases() {
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  },
-  function getSpecialCharacters() {
-    return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
-}
-]
