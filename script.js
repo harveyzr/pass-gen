79a85fb747c6dc97267db50fd057ef21d1ac592c
Zach Harvey <harveyzr@gmail.com>
2024-02-26 22:33:21 -0500

    Fixed mustpick

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ var symbols = "!@#$%^&*";
 //fuction for number of characters, and also alert if over or under chracter length 
 function generatePassword() {
   var passwordLength = parseInt(prompt("Please enter the number of chracters you want for new password. It must be more than 8 but less than 128."));
+  if (isNaN (passwordLength)) {
+    alert("password length must be a number");
+    return "Try Again!"
+  }
+  console.log(passwordLength);
   if(passwordLength < 8 || passwordLength > 128){
     alert("password length must be greater than 8, and less than 128");
     return "Try Again!"
   }
  
+  
   //Questions to add or not add 
   var lowerCases = confirm("Do you want lowercases in your password?");
   var upperCases = confirm("Do you want uppercases in your password");
@@ -29,34 +35,43 @@ function generatePassword() {
     return "Try Again!"
   }
   
-  //
+  
+
+  var guranteedCharacters = "" 
   var totalstring = ""
   if (lowerCases){
     totalstring += lowerstring
+    guranteedCharacters += lowerstring[Math.floor (Math.random () * lowerstring.length)]
   }
   if (upperCases){
     totalstring += upperstring   
+    guranteedCharacters += upperstring[Math.floor (Math.random () * upperstring.length)]
   }
   if (numbers){
     totalstring += numberstring
+    guranteedCharacters += numberstring[Math.floor (Math.random () * numberstring.length)]
   }
   if (special){
     totalstring += symbols
+    guranteedCharacters += symbols[Math.floor (Math.random () * symbols.length)]
   }
-
+  console.log (guranteedCharacters)
   console.log (totalstring)
 
   //math to pick randomly 
   var totalARR = totalstring.split ("")
-  var passString = "" 
+  var passArray = []
   console.log (totalARR)
   for(var i = 0;i < passwordLength; i++) {
-    var j =Math.floor (Math.random () * totalARR.length)
-    console.log (j) 
-    var character = totalARR[j] 
-    passString += character
+    var index =Math.floor (Math.random () * totalARR.length)
+    console.log (index) 
+    var character = totalARR[index] 
+    passArray.push (character) 
   }
-  return passString
+  for(var j = 0;j <guranteedCharacters.length;j++) {
+    passArray[j] =guranteedCharacters[j]
+  }
+  return passArray.join("")
 }
 
 
